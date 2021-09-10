3759a1cbb5dd5f9c6bc1e07446f58e975a47c723
Huon Imberger <huon@plonq.org>
2021-09-10 20:13:46 +1000

    Weapon accuracy

--- src/weapons.py
+++ src/weapons.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from src.config import config
@@ -14,18 +15,20 @@ class Weapon:
         self.reload_time = config["weapons"][type]["reload_time"]
         self.round_cooldown = config["weapons"][type]["round_cooldown"]
         self.bullet_speed = config["weapons"][type]["bullet_speed"]
+        self.accuracy = config["weapons"][type]["accuracy"]
         self.damage = config["weapons"][type]["damage"]
         self.current_cooldown = 0
         self.image = self.game.assets.basic_projectile
 
-    def update(self, dt):
+    def update(self):
         if self.current_cooldown > 0:
             self.current_cooldown -= 1
 
     def attack(self, start_pos, direction):
         if self.current_cooldown == 0:
-            jitter = random.randrange(-15, 15)
-            # direction.rotate_ip(jitter)
+            angle_offset = round((1 - self.accuracy) * 90)
+            angle = random.randint(-angle_offset, angle_offset)
+            direction.rotate_ip(angle)
             start_pos = start_pos + (direction * 14)
             self.game.world.projectiles.append(
                 Projectile(self.image, start_pos, direction)
