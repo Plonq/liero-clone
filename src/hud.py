6316374fb516c8c4ec73bca914f9f33bc3e6ba5c
Huon Imberger <huon@plonq.org>
2021-09-19 13:42:07 +1000

    colorful ammo bar

--- src/hud.py
+++ src/hud.py
@@ -7,9 +7,12 @@ from src.engine.game import GameObject
 class HUD(GameObject):
     def __init__(self, game):
         self.game = game
-        self.bar_max_width = 150
+        self.bar_max_width = 200
+        self.bar_height = 4
         self.ammo = 0
         self.reload_perc = 0
+        self.ammo_bar_zero = pg.Color(201, 40, 40)
+        self.ammo_bar_full = pg.Color(40, 72, 201)
         observe("ammo", self._update_ammo)
         observe("reload_perc", self._update_reload_perc)
 
@@ -24,9 +27,8 @@ class HUD(GameObject):
 
     def draw(self, surface, offset):
         # Ammo bar
-        rect = pg.Rect(10, 10, self.bar_max_width, 5)
-        if self.reload_perc > 0:
-            rect.width = self.bar_max_width * self.reload_perc
-        else:
-            rect.width = self.bar_max_width * self.ammo
-        pg.draw.rect(surface, (255, 255, 255), rect)
+        rect = pg.Rect(10, 10, self.bar_max_width, self.bar_height)
+        percentage = self.reload_perc if self.reload_perc > 0 else self.ammo
+        rect.width = self.bar_max_width * percentage
+        color = self.ammo_bar_zero.lerp(self.ammo_bar_full, percentage)
+        pg.draw.rect(surface, color, rect)
